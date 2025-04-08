fac8a1da1ce47ebe939b5fe22a008e0c14b2b3fd
om29-dev <ombharambe901@gmail.com>
2025-04-08 22:56:48 +0530

    `Added dashboard route and updated links to point to it`

--- App.js
+++ App.js
@@ -3,13 +3,35 @@ const path = require('path')
 
 const app = express()
 
-
 app.set('view engine', 'ejs')
-
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('pages/dashboard');
+});
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+
+app.get('/profile', (req, res) => {
+    res.render('pages/profile');
+});
+
+app.get('/project', (req, res) => {
+    res.render('pages/project');
+});
+
+app.get('/resources', (req, res) => {
+    res.render('pages/resume');
+});
+
+app.get('/progress', (req, res) => {
+    res.render('pages/progress');
 });
 
 app.use(express.static(path.join(__dirname, 'public')))
